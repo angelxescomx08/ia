1d2b237c5f61976a7a2e92cfc00a44e9a88601d3
angelxescomx08 <ah@xdevelop.mx>
2022-06-04 12:47:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ia
+ Aprendiendo machine learning
---
+++ preprocesado de datos/preprocesado_datos.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Plantilla de preprocesado
+import numpy as np #numeros
+import matplotlib.pyplot as plt #graficas
+import pandas as pd #carga de datos
