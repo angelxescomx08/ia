75b57240c094ccd4467bedffe71c223ad2d2554c
angelxescomx08 <ah@xdevelop.mx>
2022-06-05 10:05:44 -0500

    Tratamiento de na's

--- preprocesado de datos/preprocesado_datos.py
+++ preprocesado de datos/preprocesado_datos.py
@@ -14,3 +14,10 @@ import pandas as pd #carga de datos
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:,:-1].values #(filas,columnas)
 y = dataset.iloc[:,3].values
+
+#Tratamiento de Na's
+from sklearn.impute import SimpleImputer
+#Objeto para reemplazar valores indefinidos(Valores a reemplazar,forma de remplazarlos(media), filas o columnas(columnas))
+imputer = SimpleImputer(missing_values = np.nan, strategy = "mean", verbose = 0) 
+imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
