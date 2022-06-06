f13915f8a15ab5fa94c9c390d9ff9fbfdf814536
angelxescomx08 <ah@xdevelop.mx>
2022-06-05 21:14:31 -0500

    escalado de variables

--- preprocesado de datos/preprocesado_datos.py
+++ preprocesado de datos/preprocesado_datos.py
@@ -40,3 +40,20 @@ y = le_y.fit_transform(y)
 #Dividir el dataset en un conjunto de entrenamiento y un conjunto de testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) #Entradas,salidas, tamaño del test, semilla para que salgan los mismos datos
+
+#Escalado de variables
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+
+
+
+
+
+
+
+
+
